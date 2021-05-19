fb11f730c7760a8fa7078386f1e145bc9832c3c7
Jamey Kirk <jameykirk@gmail.com>
2021-05-19 17:17:57 +0900

    can't access JSON object

--- quote.js
+++ quote.js
@@ -14,14 +14,30 @@ window.addEventListener("DOMContentLoaded", function () {
 
 function fetchQuotes(topic, count) {
    // TODO: Modify to use XMLHttpRequest
-
+   let xhr = new XMLHttpRequest();
+   xhr.addEventListener("load", responseReceivedHandler);
+   xhr.responseType = "json";
+   let queryStr = "topic=" + topic.value + "&count=" + count.value;
+   xhr.open("GET", "https://wp.zybooks.com/quotes.php?" + queryStr);
+   xhr.send();
+   
    let html = "<ol>";
    for (let c = 1; c <= count; c++) {
       html += `<li>Quote ${c} - Anonymous</li>`;
    }
    html += "</ol>";
-
-   document.querySelector("#quotes").innerHTML = html;
+   //document.querySelector("#quotes").innerHTML = html; // responseReceivedHandler();
 }
 
 // TODO: Add responseReceivedHandler() here
+function responseReceivedHandler() {
+   if (this.status === 200) {
+      let quoteObj = this.response;
+      //let quoteObj = JSON.parse(test);
+      document.querySelector("#quotes").innerHTML = quoteObj.quote;
+      //this.response;
+   }
+   // else {
+
+   // }
+}
